9bf9b355b85af1197a1a089b352734daf530569d
Lenovo-User <Lenovo-User@DESKTOP-766BLG6>
2024-01-22 19:27:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+            SayHiUser("Monde");
+
+            SayHiAge("Monde ", 23);
+                Console.ReadLine();
+
+
+        }
+
+        private static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        private static void SayHiUser(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        private static void SayHiAge(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " + age);
+        }
+    }
+}
